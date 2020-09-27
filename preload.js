f09d9d7f02c2f176a5d3f8076b7ba480ab525bc1
梦宇 <xtiqin@163.com>
2020-09-27 08:46:24 +0800

    完成该项目

--- preload.js
+++ preload.js
@@ -1,12 +1,21 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type])
+    }
 })
+
+
+window.NodePath = require("path");
+window.electron = require("electron");
+window.http = require("http");
+window.util = require("util");
+window.fs = require("fs");
+window.NodeBuffer = Buffer;
+window.fsWriteAsync = window.util.promisify(fs.writeFile.bind(fs));
--- renderer.js
+++ renderer.js
@@ -4,3 +4,82 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+
+new Vue({
+    el: ".root",
+    data: {
+        m3u8_url: "",
+        download_dir: "",
+        downloading: false,
+        output: ""
+    },
+    mounted() {
+        this.read_url_to_download();
+    },
+    methods: {
+        read_url_to_download() {
+            this.m3u8_url = localStorage.getItem("m3u8_url") || "";
+            this.download_dir = localStorage.getItem("download_dir") || "";
+        },
+
+        async browseForDownloadDir() {
+            let r = await electron.remote.dialog.showOpenDialog(electron.remote.getCurrentWindow(), {properties: ["createDirectory", "openDirectory"]});
+            if (r.filePaths && r.filePaths.length) {
+                this.download_dir = r.filePaths[0];
+            }
+        },
+
+        async start_download() {
+
+            if (!this.download_dir) {
+                alert("请指定下载目录");
+                return;
+            }
+
+            if (!this.m3u8_url) {
+                alert("请指定要下载的 m3u8 文件地址");
+                return
+            }
+
+            this.downloading = true;
+            let base_url = NodePath.dirname(this.m3u8_url);
+            let m3u8_file_name = NodePath.basename(this.m3u8_url);
+            this.appendLine(`正在分析 ${this.m3u8_url}`);
+
+            let m3u8_res = await fetch(this.m3u8_url);
+            let m3u8_content = await m3u8_res.text();
+            await fsWriteAsync(NodePath.join(this.download_dir, m3u8_file_name), NodeBuffer.from(m3u8_content));
+            let lines = m3u8_content.split("\n");
+            let ts_files = lines.filter(v => v.toLowerCase().endsWith(".ts"));
+
+            this.appendLine(`分析完成 ${this.m3u8_url}`);
+            for (let f of ts_files) {
+                let tsUrl = NodePath.join(base_url, f);
+                this.appendLine(`正在下载 ${tsUrl}`);
+                let tsReq = await fetch(tsUrl);
+                let data = await tsReq.arrayBuffer();
+                await fsWriteAsync(NodePath.join(this.download_dir, f), NodeBuffer.from(data));
+                this.appendLine(`下载完成 ${tsUrl}`);
+            }
+
+            this.downloading = false;
+        },
+
+        appendLine(msg) {
+            this.output += `${msg}\n`;
+            this.$nextTick(v => {
+                this.$refs.outputTa.scrollTop = this.$refs.outputTa.scrollHeight;
+            });
+        }
+    },
+    watch: {
+        m3u8_url(v) {
+            localStorage.setItem("m3u8_url", v)
+        },
+
+        download_dir(v) {
+            localStorage.setItem("download_dir", v)
+        }
+    }
+});
